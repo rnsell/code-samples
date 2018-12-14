1dd510f32ad72143306d65a77a7a26f45b923679
Robert Sell <3444545+rnsell@users.noreply.github.com>
2018-12-14 08:44:06 -0500

    feat: add skelton of grading student page

--- src/pages/grading-students-page/grading-student.page.tsx
+++ src/pages/grading-students-page/grading-student.page.tsx
@@ -1,5 +1,26 @@
 import * as React from "react";
 
-export const GradingStudentPage = () => {
-  return <p> Hello World</p>;
-};
+interface IGradingStudentPageState {
+  grades: Number[];
+}
+
+export class GradingStudentPage extends React.Component<
+  {},
+  IGradingStudentPageState
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
+  appendGradeToArray(newGrade: Number) {
+    const { grades } = this.state;
+    this.setState({ grades: [...grades, newGrade] });
+  }
+
+  render() {
+    return <p> Hello World</p>;
+  }
+}
